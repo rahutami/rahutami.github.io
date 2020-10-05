84c1c08e9b0e18671e99ea228117e3f49888fee0
Gayuh Tri Rahutami <69070763+wundersmith@users.noreply.github.com>
2020-10-06 00:15:48 +0700

    vector calculator

--- vectorCalculator/script.js
+++ vectorCalculator/script.js
@@ -5,6 +5,11 @@ const opBtn = document.querySelector(".op-btn");
 const plBtn = document.querySelector(".pl-btn");
 const ppBtn = document.querySelector(".pp-btn");
 const plPlaneBtn = document.querySelector(".pl-plane-btn");
+const line2pBtn = document.querySelector(".line-2p-btn");
+const linePVBtn = document.querySelector(".line-pv-btn");
+const linePLBtn = document.querySelector(".line-pl-btn");
+const plane3pBtn = document.querySelector(".plane-3p-btn");
+const planePVBtn = document.querySelector(".plane-pv-btn");
 
 opBtn.addEventListener('click', countOP);
 crossBtn.addEventListener('click', countCrossProduct);
@@ -12,6 +17,11 @@ dotBtn.addEventListener('click', countDotProduct);
 plBtn.addEventListener('click', countPlDistance);
 ppBtn.addEventListener('click', countPpDistance);
 plPlaneBtn.addEventListener('click', countPlPlDistance);
+line2pBtn.addEventListener('click', countLine2P);
+linePVBtn.addEventListener('click', countLinePV);
+linePLBtn.addEventListener('click', countLinePL);
+plane3pBtn.addEventListener('click', countPlane3P);
+planePVBtn.addEventListener('click', countPlanePV);
 
 for (let i = 0; i < magBtn.length; i++){
     magBtn[i].addEventListener('click', countMagnitude);
@@ -25,6 +35,253 @@ function fpb(a,b){
     }
 
     return fpb(b, a % b);
+}
+
+function countPlanePV(e){
+    let koef = {
+        x : document.querySelector('#plane-pv-x2').value,
+        y : document.querySelector('#plane-pv-y2').value,
+        z : document.querySelector('#plane-pv-z2').value
+    }
+
+    console.log(koef.x);
+    console.log(koef.y);
+    console.log(koef.z);
+
+    if (koef.x === 0 && koef.y === 0 && koef.z === 0) {
+        e.target.parentElement.lastElementChild.innerHTML = `Undefined`;
+        e.target.parentElement.lastElementChild.classList.add("answered");
+    } else {
+        let point = {
+            x : document.querySelector('#plane-pv-x1').value,
+            y : document.querySelector('#plane-pv-y1').value,
+            z : document.querySelector('#plane-pv-z1').value
+        }
+        
+        let ans = "";
+
+        if (koef.x !== 0) ans += `${point.x}x `;
+
+        if (koef.y !== 0 && ans === "") ans += `${point.y}y `;
+        else if (koef.y > 0) ans += `+ ${point.y}y `;
+        else if (koef.y < 0) ans += `- ${Math.abs(point.y)}y `;
+
+        if (koef.z !== 0 && ans === "") ans += `${point.z}z `;
+        else if (koef.z > 0) ans += `+ ${point.z}z `;
+        else if (koef.z < 0) ans += `- ${Math.abs(point.z)}z `;
+
+        let anskoef = (-1)*(point.x * koef.x + point.y * koef.y + point.z * koef.z);
+
+        if (anskoef > 0) ans += `+ ${anskoef}`;
+        else if (anskoef < 0) ans += `- ${Math.abs(anskoef)}`;
+
+        ans += " = 0"
+
+        e.target.parentElement.lastElementChild.innerHTML = ans;
+        e.target.parentElement.lastElementChild.classList.add("answered");
+    }
+
+}
+
+function countPlane3P(e){
+    let point = {
+        x : parseFloat(document.querySelector("#plane-2p-x1").value),
+        y : parseFloat(document.querySelector("#plane-2p-y1").value),
+        z : parseFloat(document.querySelector("#plane-2p-z1").value)
+    };
+    let vec1 = {
+        x : parseFloat(document.querySelector("#plane-2p-x2").value) - point.x,
+        y : parseFloat(document.querySelector("#plane-2p-y2").value) - point.y,
+        z : parseFloat(document.querySelector("#plane-2p-z2").value) - point.z
+    };
+    let vec2 = {
+        x : parseFloat(document.querySelector("#plane-2p-x3").value) - point.x,
+        y : parseFloat(document.querySelector("#plane-2p-y3").value) - point.y,
+        z : parseFloat(document.querySelector("#plane-2p-z3").value) - point.z
+    };
+    let vec = {
+        x : vec1.y * vec2.z - vec1.z * vec2.y,
+        y : vec1.z * vec2.x - vec2.z * vec1.z,
+        z : vec1.x * vec2.y - vec1.y * vec2.x
+    }
+    let ans = {};
+    
+    if (point.x !== 0) ans.x = `${point.x} `;
+    else ans.x = "";
+    if (point.y !== 0) ans.y = `${point.y} `;
+    else ans.y = "";
+    if (point.z !== 0) ans.z = `${point.z} `;
+    else ans.z = "";
+    
+    if (vec.x > 0 && ans.x === "") ans.x += `${vec.x}t`;
+    else if (vec.x > 0) ans.x += `+ ${vec.x}t`;
+    else if (vec.x < 0) ans.x += `- ${Math.abs(vec.x)}t`;
+    else if (ans.x ==="") ans.x = `0`;
+
+    if (vec.y > 0 && ans.y === "") ans.y += `${vec.y}t`;
+    else if (vec.y > 0) ans.y += `+ ${vec.y}t`;
+    else if (vec.y < 0) ans.y += `- ${Math.abs(vec.y)}t`;
+    else if (ans.y ==="") ans.y = `0`;
+
+    if (vec.z > 0 && ans.z === "") ans.z += `${vec.z}t`;
+    else if (vec.z > 0) ans.z += `+ ${vec.z}t`;
+    else if (vec.z < 0) ans.z += `- ${Math.abs(vec.z)}t`;
+    else if (ans.z ==="") ans.z = `0`;
+
+    e.target.parentElement.lastElementChild.innerHTML = `Parametric Equations:<br>x = ${ans.x}<br>y = ${ans.y}<br>z = ${ans.z}`;
+    e.target.parentElement.lastElementChild.classList.add("answered");
+}
+
+function countLinePL(e) {
+    let n1 = {
+        x : parseFloat(document.querySelector("#line-pl-x1").value),
+        y : parseFloat(document.querySelector("#line-pl-y1").value),
+        z : parseFloat(document.querySelector("#line-pl-z1").value)
+    }
+    let n2 = {
+        x : parseFloat(document.querySelector("#line-pl-x2").value),
+        y : parseFloat(document.querySelector("#line-pl-y2").value),
+        z : parseFloat(document.querySelector("#line-pl-z2").value)
+    };
+
+    if(n1.x !== n2.x || n1.y !== n2.y || n1.z !== n2.z){
+
+        let vec = {
+            x : n1.y * n2.z - n1.z * n2.y,
+            y : n1.z * n2.x - n2.z * n1.z,
+            z : n1.x * n2.y - n1.y * n2.x
+        }
+
+        let point = {
+            x : 0,
+            y1 : n1.y,
+            y2 : n2.y,
+            z1 : n1.z,
+            z2 : n2.z,
+            k1 : parseFloat(document.querySelector("#line-pl-k1").value),
+            k2 : parseFloat(document.querySelector("#line-pl-k2").value)
+        }
+
+        point.k1 /= point.y1;
+        point.z1 /= point.y1;
+        point.y1 /= point.y1;
+
+        point.k2 -= (point.y2/point.y1)*point.k1;
+        point.z2 -= (point.y2/point.y1)*point.z1;
+        point.y2 = 0;
+
+        point.k1 -= (point.z1/point.z2)*point.k2;
+        point.z2 = 0;
+
+        let ans = {};
+        
+        if (point.x !== 0) ans.x = `${+point.x.toFixed(2)} `;
+        else ans.x = "";
+        if (point.k1 !== 0) ans.y = `${+point.k1.toFixed(2)} `;
+        else ans.y = "";
+        if (point.k2 !== 0) ans.z = `${+point.k2.toFixed(2)} `;
+        else ans.z = "";
+        
+        if (vec.x > 0 && ans.x === "") ans.x += `${vec.x}t`;
+        else if (vec.x > 0) ans.x += `+ ${vec.x}t`;
+        else if (vec.x < 0) ans.x += `- ${Math.abs(vec.x)}t`;
+        else if (ans.x ==="") ans.x = `0`;
+
+        if (vec.y > 0 && ans.y === "") ans.y += `${vec.y}t`;
+        else if (vec.y > 0) ans.y += `+ ${vec.y}t`;
+        else if (vec.y < 0) ans.y += `- ${Math.abs(vec.y)}t`;
+        else if (ans.y ==="") ans.y = `0`;
+
+        if (vec.z > 0 && ans.z === "") ans.z += `${vec.z}t`;
+        else if (vec.z > 0) ans.z += `+ ${vec.z}t`;
+        else if (vec.z < 0) ans.z += `- ${Math.abs(vec.z)}t`;
+        else if (ans.z ==="") ans.z = `0`;
+
+        e.target.parentElement.lastElementChild.innerHTML = `Parametric Equations:<br>x = ${ans.x}<br>y = ${ans.y}<br>z = ${ans.z}`;
+        e.target.parentElement.lastElementChild.classList.add("answered");
+    } else {
+        e.target.parentElement.lastElementChild.innerHTML = `Planes are parallel to each other`;
+        e.target.parentElement.lastElementChild.classList.add("answered");
+    }
+}
+
+function countLinePV(e) {
+    let point = {
+        x : parseFloat(document.querySelector("#line-pv-x1").value),
+        y : parseFloat(document.querySelector("#line-pv-y1").value),
+        z : parseFloat(document.querySelector("#line-pv-z1").value)
+    }
+    let vec = {
+        x : parseFloat(document.querySelector("#line-pv-x2").value),
+        y : parseFloat(document.querySelector("#line-pv-y2").value),
+        z : parseFloat(document.querySelector("#line-pv-z2").value)
+    };
+
+    let ans = {};
+    
+    if (point.x !== 0) ans.x = `${point.x} `;
+    else ans.x = "";
+    if (point.y !== 0) ans.y = `${point.y} `;
+    else ans.y = "";
+    if (point.z !== 0) ans.z = `${point.z} `;
+    else ans.z = "";
+    
+    if (vec.x > 0 && ans.x === "") ans.x += `${vec.x}t`;
+    else if (vec.x > 0) ans.x += `+ ${vec.x}t`;
+    else if (vec.x < 0) ans.x += `- ${Math.abs(vec.x)}t`;
+    else if (ans.x ==="") ans.x = `0`;
+
+    if (vec.y > 0 && ans.y === "") ans.y += `${vec.y}t`;
+    else if (vec.y > 0) ans.y += `+ ${vec.y}t`;
+    else if (vec.y < 0) ans.y += `- ${Math.abs(vec.y)}t`;
+    else if (ans.y ==="") ans.y = `0`;
+
+    if (vec.z > 0 && ans.z === "") ans.z += `${vec.z}t`;
+    else if (vec.z > 0) ans.z += `+ ${vec.z}t`;
+    else if (vec.z < 0) ans.z += `- ${Math.abs(vec.z)}t`;
+    else if (ans.z ==="") ans.z = `0`;
+
+    e.target.parentElement.lastElementChild.innerHTML = `Parametric Equations:<br>x = ${ans.x}<br>y = ${ans.y}<br>z = ${ans.z}`;
+    e.target.parentElement.lastElementChild.classList.add("answered");
+}
+function countLine2P(e) {
+    let point = {
+        x : parseFloat(document.querySelector("#line-2p-x2").value),
+        y : parseFloat(document.querySelector("#line-2p-y2").value),
+        z : parseFloat(document.querySelector("#line-2p-z2").value)
+    }
+    let vec = {
+        x : point.x - parseFloat(document.querySelector("#line-2p-x1").value),
+        y : point.y - parseFloat(document.querySelector("#line-2p-y1").value),
+        z : point.z - parseFloat(document.querySelector("#line-2p-z1").value)
+    };
+
+    let ans = {};
+    
+    if (point.x !== 0) ans.x = `${point.x} `;
+    else ans.x = "";
+    if (point.y !== 0) ans.y = `${point.y} `;
+    else ans.y = "";
+    if (point.z !== 0) ans.z = `${point.z} `;
+    else ans.z = "";
+    
+    if (vec.x > 0 && ans.x === "") ans.x += `${vec.x}t`;
+    else if (vec.x > 0) ans.x += `+ ${vec.x}t`;
+    else if (vec.x < 0) ans.x += `- ${Math.abs(vec.x)}t`;
+    else if (ans.x ==="") ans.x = `0`;
+
+    if (vec.y > 0 && ans.y === "") ans.y += `${vec.y}t`;
+    else if (vec.y > 0) ans.y += `+ ${vec.y}t`;
+    else if (vec.y < 0) ans.y += `- ${Math.abs(vec.y)}t`;
+    else if (ans.y ==="") ans.y = `0`;
+
+    if (vec.z > 0 && ans.z === "") ans.z += `${vec.z}t`;
+    else if (vec.z > 0) ans.z += `+ ${vec.z}t`;
+    else if (vec.z < 0) ans.z += `- ${Math.abs(vec.z)}t`;
+    else if (ans.z ==="") ans.z = `0`;
+
+    e.target.parentElement.lastElementChild.innerHTML = `Parametric Equations:<br>x = ${ans.x}<br>y = ${ans.y}<br>z = ${ans.z}`;
+    e.target.parentElement.lastElementChild.classList.add("answered");
 }
 
 function countPlDistance(e){
